a27f5b8d9c5971b6fddf43e33423ba467b72b095
smalaca <ab13.krakow@gmail.com>
2016-12-15 19:46:49 +0100

    Refactoring Patterns - extract temporary variable

--- src/main/java/com/smalaca/refactoring/apply/ApplyRefactoring.java
+++ src/main/java/com/smalaca/refactoring/apply/ApplyRefactoring.java
@@ -3,10 +3,12 @@ package com.smalaca.refactoring.apply;
 import com.smalaca.refactoring.domain.ClassCode;
 import com.smalaca.refactoring.domain.Condition;
 import com.smalaca.refactoring.domain.Refactoring;
+import com.smalaca.refactoring.domain.RefactoringScope;
 
 public class ApplyRefactoring {
     public void apply(ClassCode code, Refactoring refactoring) {
-        Condition condition = code.getMethodByName(refactoring.getScope()).getCondition();
+        RefactoringScope refactoringScope = refactoring.getScope();
+        Condition condition = code.getMethodByName(refactoringScope).getCondition();
         RefactoringApplier.applyRefactoringFor(condition, refactoring);
     }
 }
