8311a7fd3f7ab50177eafa1ed7ff9dff720aaf7b
smalaca <ab13.krakow@gmail.com>
2016-12-15 21:25:04 +0100

    introduce CreditService

---
+++ src/main/java/com/smalaca/bank/credit/CreditService.java
@@ -0,0 +1,18 @@
+package com.smalaca.bank.credit;
+
+import com.smalaca.bank.domain.Client;
+import com.smalaca.bank.domain.Offer;
+
+public class CreditService {
+    Offer calculateOfferFor12MonthsFor(Client client) {
+        return null;
+    }
+
+    Offer calculateOfferFor60MonthsFor(Client client) {
+        return null;
+    }
+
+    Offer calculateOfferFor120MonthsFor(Client client) {
+        return null;
+    }
+}
---
+++ src/main/java/com/smalaca/bank/domain/Offer.java
@@ -0,0 +1,4 @@
+package com.smalaca.bank.domain;
+
+public class Offer {
+}
