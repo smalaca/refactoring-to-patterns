f112578836176501f73fa8e633c645a753fc9d17
smalaca <ab13.krakow@gmail.com>
2016-12-15 20:48:25 +0100

    introduce sex and contact persons into client

--- src/main/java/com/smalaca/bank/domain/Client.java
+++ src/main/java/com/smalaca/bank/domain/Client.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
+    public static final int MALE = 1;
+    public static final int FEMALE = 0;
+
     private final String firstName;
     private final String lastName;
+    private final int sex;
     private String city;
     private String street;
     private String houseNumber;
     private List<String> phoneNumbers = new ArrayList<>();
+    private List<ContactPerson> contacts = new ArrayList<>();
 
-    Client(String firstName, String lastName) {
+    Client(String firstName, String lastName, int sex) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.sex = sex;
     }
 
     void changeAddress(String houseNumber, String street, String city) {
