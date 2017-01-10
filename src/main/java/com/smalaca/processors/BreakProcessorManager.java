7bef832d8eef2389a49880e3cf196092dc34b5d3
smalaca <ab13.krakow@gmail.com>
2017-01-10 06:49:40 +0100

    Introduce BreakProcessorManager

---
+++ src/main/java/com/smalaca/processors/BreakProcessorManager.java
@@ -0,0 +1,23 @@
+package com.smalaca.processors;
+
+import com.smalaca.domain.InputParameter;
+
+import java.util.List;
+
+class BreakProcessorManager {
+    private final List<Processor> processors;
+
+    BreakProcessorManager(List<Processor> processors) {
+        this.processors = processors;
+    }
+
+    void process(InputParameter inputParameter) {
+        for (Processor processor : processors) {
+            if (processor.isApplicableFor(inputParameter)) {
+                processor.process(inputParameter);
+                break;
+            }
+        }
+    }
+
+}
