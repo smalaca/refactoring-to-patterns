bd4690ca9bfcd090d8e30c4a2fe6e29343c4a243
smalaca <ab13.krakow@gmail.com>
2017-03-23 19:22:07 +0100

    Refactoring - introduce a first step

--- src/main/java/com/smalaca/designpatterns/example/chainofresponsibility/alertcenter/AlertCenter.java
+++ src/main/java/com/smalaca/designpatterns/example/chainofresponsibility/alertcenter/AlertCenter.java
@@ -5,41 +5,18 @@ import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.home
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.home.Fridge;
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.home.Thermostat;
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.monitoring.TemperatureMonitoringCenter;
-import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.shop.Products;
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.shop.Shop;
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.temperaturemonitoring.TemperatureAlerts;
 import com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.temperaturemonitoring.TemperatureControl;
 
-import static com.smalaca.designpatterns.example.chainofresponsibility.alertcenter.shop.Order.anOrder;
-
 class AlertCenter {
-    private static final Temperature TOO_LOW_TEMPERATURE = new Temperature(-30);
-
-    private final Thermostat thermostat;
-    private final Shop shop;
     private final TemperatureControl temperatureControl;
 
     AlertCenter(TemperatureMonitoringCenter temperatureMonitoringCenter, Thermostat thermostat, Shop shop, Fridge fridge, Fan fan) {
-        this.thermostat = thermostat;
-        this.shop = shop;
-
-        temperatureControl = new TemperatureAlerts(temperatureMonitoringCenter, shop, fridge, fan).alerts();
+        temperatureControl = new TemperatureAlerts(temperatureMonitoringCenter, thermostat, shop, fridge, fan).alerts();
     }
 
     void verify(Temperature temperature) {
-        if (temperature.lowerThan(TOO_LOW_TEMPERATURE)) {
-            increaseHeating();
-            orderLotOfHotTee();
-        } else {
-            temperatureControl.reactOn(temperature);
-        }
-    }
-
-    private void increaseHeating() {
-        thermostat.increaseTemperature();
-    }
-
-    private void orderLotOfHotTee() {
-        shop.order(anOrder(Products.TEE));
+        temperatureControl.reactOn(temperature);
     }
 }
