eeafe04fb4d5f4f7b1d80625ef84e8bdce125b79
smalaca <ab13.krakow@gmail.com>
2017-03-24 06:58:15 +0100

    BEFORE REFACTORING

--- src/main/java/com/smalaca/designpatterns/example/visitor/message/send/Messanger.java
+++ src/main/java/com/smalaca/designpatterns/example/visitor/message/send/Messanger.java
@@ -1,19 +1,42 @@
 package com.smalaca.designpatterns.example.visitor.message.send;
 
+import com.smalaca.designpatterns.example.visitor.message.domain.Email;
 import com.smalaca.designpatterns.example.visitor.message.domain.Message;
+import com.smalaca.designpatterns.example.visitor.message.domain.Sms;
+import com.smalaca.designpatterns.example.visitor.message.domain.Tweet;
+import com.smalaca.designpatterns.example.visitor.message.domain.UnsupportedMessageException;
 import com.smalaca.designpatterns.example.visitor.message.domain.repository.MessageRepository;
 
 public class Messanger {
-    private final MessageSender messageSender;
     private final MessageRepository messageRepository;
 
-    public Messanger(MessageSender messageSender, MessageRepository messageRepository) {
-        this.messageSender = messageSender;
+    public Messanger(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
     void send(Message message) {
-        message.accept(messageSender);
+        if (message instanceof Email) {
+            sendAnEmail((Email) message);
+        } else if (message instanceof Tweet) {
+            sendTweet((Tweet) message);
+        } else if (message instanceof Sms) {
+            sendSms((Sms) message);
+        } else {
+            throw new UnsupportedMessageException();
+        }
+
         messageRepository.store(message);
+    }
+
+    private void sendAnEmail(Email email) {
+
+    }
+
+    private void sendTweet(Tweet tweet) {
+
+    }
+
+    private void sendSms(Sms sms) {
+
     }
 }
--- src/main/java/com/smalaca/designpatterns/example/visitor/message/statistics/Statistics.java
+++ src/main/java/com/smalaca/designpatterns/example/visitor/message/statistics/Statistics.java
@@ -1,21 +1,45 @@
 package com.smalaca.designpatterns.example.visitor.message.statistics;
 
+import com.smalaca.designpatterns.example.visitor.message.domain.Email;
 import com.smalaca.designpatterns.example.visitor.message.domain.Message;
+import com.smalaca.designpatterns.example.visitor.message.domain.Sms;
 import com.smalaca.designpatterns.example.visitor.message.domain.Statistic;
+import com.smalaca.designpatterns.example.visitor.message.domain.Tweet;
+import com.smalaca.designpatterns.example.visitor.message.domain.UnsupportedMessageException;
 import com.smalaca.designpatterns.example.visitor.message.domain.repository.StatisticsRepository;
 
 public class Statistics {
     private final StatisticsRepository statisticsRepository;
-    private final StatisticsAnalyzer statisticsAnalyzer;
 
-    public Statistics(StatisticsRepository statisticsRepository, StatisticsAnalyzer statisticsAnalyzer) {
+    public Statistics(StatisticsRepository statisticsRepository) {
         this.statisticsRepository = statisticsRepository;
-        this.statisticsAnalyzer = statisticsAnalyzer;
     }
 
     void gather(Message message) {
-        message.accept(statisticsAnalyzer);
-        Statistic statistic = statisticsAnalyzer.getLast();
+        Statistic statistic;
+
+        if (message instanceof Email) {
+            statistic = gatherFromEmail((Email) message);
+        } else if (message instanceof Tweet) {
+            statistic = gatherFromTweet((Tweet) message);
+        } else if (message instanceof Sms) {
+            statistic = gatherFromSms((Sms) message);
+        } else {
+            throw new UnsupportedMessageException();
+        }
+
         statisticsRepository.store(statistic);
+    }
+
+    private Statistic gatherFromEmail(Email email) {
+        return null;
+    }
+
+    private Statistic gatherFromTweet(Tweet tweet) {
+        return null;
+    }
+
+    private Statistic gatherFromSms(Sms sms) {
+        return null;
     }
 }
